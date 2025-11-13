ae31620def3571f74ae750d7754cc2802f9cf604
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-11-13 11:23:28 -0300

    fix: fixed SQLite path and message at server start

--- src/config/database.js
+++ src/config/database.js
@@ -6,10 +6,10 @@ let dbInstance = null;
 function getConnectionDB() {
 	if (dbInstance) return dbInstance;
 
-	const dbPath = path.resolve(__dirname, "../database/database.sqlite");
+	const dbPath = path.resolve(__dirname, "../db/database.sqlite");
 	dbInstance = new sqlite3.Database(dbPath, (err) => {
 		if (err) console.error("Erro ao conectar ao SQLite:", err.message);
-		else console.log("Conectado ao banco de dados SQLite.");
+		else console.log("[SQLite] Conectado em:", dbPath);
 	});
 
 	return dbInstance;
