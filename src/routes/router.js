26f99693ee2bcc6a5077fdbeeca2860754d46acd
gabrildnsilva <gabrieldnsilva@gmail.com>
2025-11-13 11:31:29 -0300

    routes: Router now has register / validate routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const userController = require("../controllers/userController");
 
 router.get("/", (_req, res) => res.json({ ok: true }));
+
+router.post("/register", (req, res) => userController.register(req, res));
+router.post("/validate", (req, res) => userController.validate(req, res));
 
 module.exports = router;
